a1690c886f8161cff59a518be73c9fe10f97d3d1
jschl <Julian.Schlimme@web.de>
2021-01-24 13:48:24 +0100

    add recipe model

--- recipe_api/models.py
+++ recipe_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=60)
+    ingredients = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
